2845adef9ac107511e19d8a3c22260b910af1b3e
pktomlinson <peter@pktomlinson.ca>
2019-10-13 09:18:19 -0400

    getth_office.py
    read_sensor now accepts inputs for sensor id and pin #

--- getth_office.py
+++ getth_office.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
+# import required modules and libraries
 from datetime import datetime
 import Adafruit_DHT
 import paho.mqtt.client as mqtt
-import json
 import string
 import random
 
+# generate a random clientId for connecting to mqtt broker
 def generate_clientId():
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for i in range(8))
 
-def read_sensor():
-    #pin = 4
-    #humidity, temperature = Adafruit_DHT.read_retry(str(sensor), pin)
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+# read_sensor - requires sensor object and pin #
+def read_sensor(sensor, pin):
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     temperature = round(temperature, 1)
     humidity = round(humidity, 1)
     fdatatime = (((datetime.now() - datetime(1970, 1, 1)).total_seconds()) + 14400)
@@ -22,14 +22,22 @@ def read_sensor():
     ret = '{ "datetime:", "' + sdatatime + '", "temperature:"' + str(temperature) +'", "humidity:" , "' + str(humidity) + '" }'
     return(ret)
 
+# main - generates cliendId
+#        creates client object
+#        connects to local broker
+#        reads sensor
+#        publish returned json data
+#        disconnects from broker
 def main():
     clientId = generate_clientId()
     client = mqtt.Client(clientId)
-    client.connect("192.168.1.78", 1883)
-    data = read_sensor()
-    # print(data)
+    client.connect("localhost", 1883)
+    data = read_sensor(Adafruit_DHT.AM2302, 4)
+    print(data)
     client.publish("weather/office", str(data), qos = 0, retain = True)
-    
+    client.disconnect()
+
+# init program
 if __name__=='__main__':
     main()
 
